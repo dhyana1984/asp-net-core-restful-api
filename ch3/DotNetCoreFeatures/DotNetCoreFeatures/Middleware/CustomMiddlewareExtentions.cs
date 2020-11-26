56503539d4dc0143c19148d995db9ccc672f85e1
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-11-27 01:07:07 +0800

    Middleware

---
+++ ch3/DotNetCoreFeatures/DotNetCoreFeatures/Middleware/CustomMiddlewareExtentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace DotNetCoreFeatures.Middleware
+{
+    //扩展IApplicationBuilder, 使用UseHttpMethodCheckMiddleware更加方便
+    public static class CustomMiddlewareExtentions
+    {
+        public static IApplicationBuilder UseHttpMethodCheckMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpMethodCheckMiddleware>();
+        }
+    }
+}
---
+++ ch3/DotNetCoreFeatures/DotNetCoreFeatures/Middleware/HttpMethodCheckMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace DotNetCoreFeatures.Middleware
+{
+    //自定义中间件
+    public class HttpMethodCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public HttpMethodCheckMiddleware(RequestDelegate requestDelegate, IWebHostEnvironment environment)
+        {
+            this._next = requestDelegate;
+        }
+
+        //需要实现Invoke方法
+        public Task Invoke(HttpContext context)
+        {
+            var requestMethod = context.Request.Method.ToUpper();
+            //过滤请求的method，只允许get和head
+            if(requestMethod == HttpMethods.Get || requestMethod == HttpMethods.Head)
+            {
+                return _next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Headers.Add("X-AllowHTTPVerb", new[] { "GET, HEAD" });
+                context.Response.WriteAsync("only support GET and HEAD http methods");
+                Console.WriteLine($"request was rejected because method is {context.Request.Method}");
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
