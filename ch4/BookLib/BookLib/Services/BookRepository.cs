69547c6dff1958fd0acdb28e6363c82772f8205c
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-12-01 02:10:43 +0800

    Book Lib Get Resource

---
+++ ch4/BookLib/BookLib/Services/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookLib.Models;
+using Library.API.Data;
+
+namespace BookLib.Services
+{
+    public class BookRepository: IBookRepository
+    {
+        public BookRepository()
+        {
+        }
+
+        public BookDto GetBookForAuthor(Guid authorId, Guid bookId)
+        {
+            return LibraryMockData.Current.Books.FirstOrDefault(b => b.AuthorId == authorId && b.Id == bookId);
+        }
+
+        public IEnumerable<BookDto> GetBooksForAuthor(Guid authorId)
+        {
+            return LibraryMockData.Current.Books.Where(b => b.AuthorId == authorId).ToList();
+        }
+    }
+}
---
+++ ch4/BookLib/BookLib/Services/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using BookLib.Models;
+
+namespace BookLib.Services
+{
+    public interface IAuthorRepository
+    {
+        IEnumerable<AuthorDto> GetAuthors();
+        AuthorDto GetAuthor(Guid authorId);
+        bool IsAuthorExists(Guid authorId);
+    }
+}
